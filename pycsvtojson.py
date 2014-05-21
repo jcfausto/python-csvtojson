fb9b5dac915a1d7979ff359a67e969891ada2c53
Julio Cesar Fausto <jcfausto@gmail.com>
2014-05-20 22:42:39 -0300

    Update pycsvtojson.py
    Python 3.x compatibility

--- pycsvtojson.py
+++ pycsvtojson.py
@@ -1,3 +1,6 @@
+#python 3.x compatibility
+from __future__ import print_function
+# regular imports
 import sys
 import csv
 import json
@@ -14,47 +17,47 @@ class Pycsvtojson:
 				if argv[1] == '-v':
 					verbose = True
 
-			if verbose: print "Creating translator"
+			if verbose: print("Creating translator")
 
 			translator = Translator()
 			
-			if verbose: print "Translator created"
+			if verbose: print("Translator created")
 
-			if verbose: print "Adding dictionaries"
+			if verbose: print("Adding dictionaries")
 			translator.setDictionaries(DICTIONARIES)
-			if verbose: print "Dictionaries added"	
+			if verbose: print("Dictionaries added")	
 
-			if verbose: print "Adding fieldnames"
+			if verbose: print("Adding fieldnames")
 
 			translator.setFieldnames(FIELDNAMES)
-			if verbose: print "Fieldnames added"	
+			if verbose: print("Fieldnames added")	
 
 			inputfile = argv[0]
-			if verbose: print "Input file to be readed: " + inputfile
+			if verbose: print("Input file to be readed: " + inputfile)
 
-			if verbose: print "Openning input file"
+			if verbose: print("Openning input file")
 			csvfile = open(inputfile, 'r')
-			if verbose: print "Input file successfully opened"
+			if verbose: print("Input file successfully opened")
 
-			if verbose: print "Creating CSV reader"
+			if verbose: print("Creating CSV reader")
 			reader = csv.reader(csvfile)
-			if verbose: print "CSV reader created successfully"
+			if verbose: print("CSV reader created successfully")
 
-			if verbose: print "Creating output file"
+			if verbose: print("Creating output file")
 			jsonfile = open('output.json', 'w')
-			if verbose: print "Output file created"
+			if verbose: print("Output file created")
 
-			if verbose: print "Generating output"
+			if verbose: print("Generating output")
 			reader = csv.DictReader(csvfile, FIELDNAMES)
 			for row in reader:
 				translator.translate(row)
 				json.dump(row, jsonfile)
 				jsonfile.write('\n')
-			if verbose: print "Output generated"
+			if verbose: print("Output generated")
 
-			if verbose: print "SUCCESS!"
+			if verbose: print("SUCCESS!")
 		except:
-			print "FAIL"
+			print("FAIL")
 
 if __name__ == "__main__":
 	oCsv = Pycsvtojson()
